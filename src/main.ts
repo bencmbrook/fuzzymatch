fec96dd70147440f65e7af55cbd0f595b1c4b91b
bencmbrook <ben@transcend.io>
2022-07-24 12:32:13 -0400

    prettier

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,2 @@
-export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
+import { distance, closest} from 'fastest-levenshtein';
 
-export const greet = (name: string): string => `Hello ${name}`
-
-export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
-}
